10f758e94e387733124b02533b29d9ec5e823196
Misael Taveras <macrot29@gmail.com>
2019-10-02 10:25:03 -0400

    feat: reimplement design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PostListComponent } from './components/post-list/post-list.component';
+import { PostComponent } from './components/post/post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostListComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule
